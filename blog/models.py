111166667a16e9c9f7e7585e9969bf18df6b6ccc
Kimmi Kumari <kimmi@Kimmi-Kumari-L.local>
2021-11-12 15:46:07 +0530

    Added jobs page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.fields import CharField, DateField
+from django.db.models.fields.files import ImageField
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateField()
+    body = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
